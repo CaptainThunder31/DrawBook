b9af20c16c6131464a8fb4d91fbbbb3f0df3c784
CaptainThunder31 <your-verified-email@example.com>
2025-06-06 23:03:40 +0530

    Update 0.2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
+  import Header from './components/Header.vue'
   
+  const title = "Rps Game";
 </script>
 <template>
-  
+  <Header :title="title" />
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,9 @@
 <script setup>
+  const props = defineProps(['title']);
+  
+  const title = props.title;
   
 </script>
 <template>
-  
+  <h1>{{title}}</h1>
 </template>
