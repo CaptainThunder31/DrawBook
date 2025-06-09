4c1e4f22c4a5bd288376b90fb85b2801ca48c600
CaptainThunder31 <your-verified-email@example.com>
2025-06-09 09:00:46 +0530

    Update 0.2.1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,22 @@
 <script setup>
   const props = defineProps(['title']);
-  
+
   const title = props.title;
-  
+
 </script>
 <template>
-  <h1>{{title}}</h1>
+  <div>
+    <h1>{{title}}</h1>
+  </div>
 </template>
+<style scoped>
+  div {
+    width: 100vh;
+    height: 70px;
+    position: fixed;
+    background: #f00;
+    color: #fff;
+    box-shadow: 0 0 100px #f00;
+  }
+
+</style>
